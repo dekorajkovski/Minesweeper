962306079feac62fa27bd61be13fa593281dd9d3
Dejan Rajkovski <dekorajkovski@hotmail.com>
2016-04-17 01:30:14 +0200

    Minesweeper start

---
+++ Minesweeper/MIneField.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public class MineField : PictureBox
+    {
+        public enum Status {
+            uncovered,flagged,unknown,normal
+        }
+        public MineField up { get; set; }
+        public MineField down { get; set; }
+        public MineField left { get; set; }
+        public MineField right { get; set; }
+
+        public Boolean isBomb { get; set; }
+
+        public Status status { get; set; }
+
+        public MineField():base() {
+            
+            up = down = left = right = null;
+            isBomb = false;
+            status = Status.normal;
+        }
+        
+    }
+}
---
+++ Minesweeper/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    static class Program
+    {
+        public static MineField[][] fields=new MineField[20][];
+
+        public static void recursiveSearch() {
+
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                fields[i] = new MineField[20];
+                for (int j = 0; j < 20; j++)
+                {
+                    fields[i][j] = new MineField();
+                }
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
