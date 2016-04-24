14fb694b441dbbd2ebf1b040ac80c81e27157027
Dejan Rajkovski <dekorajkovski@hotmail.com>
2016-04-24 15:20:49 +0200

    fix for Limit of fields and the bomb you click is now red

--- Minesweeper/Program.cs
+++ Minesweeper/Program.cs
@@ -56,6 +56,7 @@ namespace Minesweeper
                 else {
                     if (field.isBomb)
                     {
+                        field.BackColor = Color.Red;
                         field.uncover("bomb");
                         Program.canPlay = false;
                         uncoverBombs();
@@ -121,8 +122,8 @@ namespace Minesweeper
         public static void setLimits(int li) {
             connect();
             for (int i = 0; i < li; i++) {
-                fields[i][li-1].right = null;
-                fields[li-1][i].right = null;
+                fields[i][li].right = null;
+                fields[li][i].right = null;
 
             }
 
@@ -186,6 +187,8 @@ namespace Minesweeper
                 if (i < bombs) ar[i] = true;
                 else ar[i] = false;
             }
+            Shuffle(ar);
+            Shuffle(ar);
             Shuffle(ar);
             int k = 0;
             for (int i = 0; i < rows; i++)
